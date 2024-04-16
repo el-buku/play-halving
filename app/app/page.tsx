f682e9c9a2893728fbddc16777bf485ce8cbe8f7
el-buku <56129429+el-buku@users.noreply.github.com>
2024-04-16 21:42:31 +0300

    gitignore idl back

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -3,20 +3,17 @@ import {
   WalletDisconnectButtonDynamic,
   WalletMultiButtonDynamic,
 } from "@/components/WalletMultiButton";
-
+import HTMLR from "react-html-renderer";
 import React from "react";
 
 //@ts-ignore
-// import appHtml from "./homepage.html";
-import { Homepage } from "@/components/Homepage";
+import appHtml from "./homepage.html";
+// import { Homepage } from "@/components/Homepage";
 import { seeds } from "../../client/config";
 
 export default function Home() {
   console.log(seeds);
-  return (
-    <body>
-      <WalletMultiButtonDynamic />;
-    </body>
-  );
+  return <HTMLR html={appHtml} />;
+
   // return <Homepage />;
 }
