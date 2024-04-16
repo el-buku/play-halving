bb91f5f401d361b141342a4d8bea8e84ddc7f348
el-buku <56129429+el-buku@users.noreply.github.com>
2024-04-16 17:37:00 +0300

    more sol

--- app/components/context/ProgramContext.tsx
+++ app/components/context/ProgramContext.tsx
@@ -7,7 +7,6 @@ import {
   useMemo,
   useState,
 } from "react";
-import { ProgramProvider } from "../../../client/provider";
 import {
   useAnchorWallet,
   useConnection,
@@ -15,11 +14,25 @@ import {
 } from "@solana/wallet-adapter-react";
 import * as anchor from "@coral-xyz/anchor";
 import { IDL, PlayHalving } from "../../../target/types/play_halving";
-import { ConfirmedSignatureInfo, PublicKey } from "@solana/web3.js";
+import {
+  ConfirmedSignatureInfo,
+  PublicKey,
+  SystemProgram,
+} from "@solana/web3.js";
 import { bettingMint } from "../../../client/config";
 import { SolanaParser } from "@debridge-finance/solana-transaction-parser";
+import {
+  getProgramConfigPDADef,
+  getSecondStateAcc,
+  getUserStateAcc,
+} from "../../../client/PDAs";
+import {
+  TOKEN_PROGRAM_ID,
+  getAssociatedTokenAddressSync,
+} from "@solana/spl-token";
 
-const ProgramContext = createContext<ProgramProvider | undefined>(undefined);
+type ProgramFns = {};
+const ProgramContext = createContext<ProgramFns | undefined>(undefined);
 
 // TODO
 const programAddy = "AwhF2my6A4mmpBBSP2UAWFo4392DY6Vp6TdrP1uFPCvu";
@@ -69,19 +82,59 @@ export const ProgramContextProvider = ({ children }: PropsWithChildren<{}>) => {
       programId,
       anchorProvider
     );
-    const programProvider = new ProgramProvider(
-      anchorProvider,
-      connection,
-      programId,
-      program,
-      IDL,
-      bettingMint
-    );
+    const buyTicketsIxn = (numTickets: number) => {
+      const programConfigPDA = getProgramConfigPDADef(programId)[0];
+      const programVault = getAssociatedTokenAddressSync(
+        bettingMint,
+        programConfigPDA,
+        true
+      );
+      const userStateAcc = getUserStateAcc(
+        wallet.publicKey,
+        TOKEN_PROGRAM_ID
+      )[0];
+      const buyerAta = getAssociatedTokenAddressSync(
+        bettingMint,
+        wallet.publicKey,
+        false,
+        TOKEN_PROGRAM_ID
+      );
+      return program.methods.buyTickets(numTickets).accounts({
+        buyer: wallet.publicKey,
+        buyerAta,
+        programVault,
+        programConfig: programConfigPDA,
+        bettingMint,
+        userStateAcc,
+        systemProgram: SystemProgram.programId,
+        associatedTokenProgram: TOKEN_PROGRAM_ID,
+      }).instruction;
+    };
+    const placeBetIxn = (timestamp: number) => {
+      const programConfigPDA = getProgramConfigPDADef(programId)[0];
+      const programVault = getAssociatedTokenAddressSync(
+        bettingMint,
+        programConfigPDA,
+        true
+      );
+      const userStateAcc = getUserStateAcc(
+        wallet.publicKey,
+        TOKEN_PROGRAM_ID
+      )[0];
+      const secondStateAcc = getSecondStateAcc(timestamp, program.programId)[0];
+
+      return program.methods.placeBet(new anchor.BN(timestamp)).accounts({
+        buyer: wallet.publicKey,
+        programConfig: programConfigPDA,
+        userStateAcc,
+        secondStateAcc,
+        systemProgram: SystemProgram.programId,
+      }).instruction;
+    };
+    const reclaimIxn = () => {};
 
     return (
-      <ProgramContext.Provider value={programProvider}>
-        {children}
-      </ProgramContext.Provider>
+      <ProgramContext.Provider value={{}}>{children}</ProgramContext.Provider>
     );
   }
 };
