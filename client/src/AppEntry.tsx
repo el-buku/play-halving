28b8efdaa9fec61d0795b97cf042fcdd941176a6
el-buku <56129429+el-buku@users.noreply.github.com>
2024-04-17 18:13:14 +0300

    slider fixed

--- client/src/AppEntry.tsx
+++ client/src/AppEntry.tsx
@@ -1,9 +1,11 @@
+import { Slider } from "./components/Slider";
 import { WalletButtonEntry } from "./components/WalletButton";
 import { AppContext } from "./context";
 
 export const AppEntry = () => {
   return (
     <AppContext>
+      <Slider container="#sliderContainer" />
       <WalletButtonEntry container="#walletButton" />
     </AppContext>
   );
--- client/src/components/Slider.tsx
+++ client/src/components/Slider.tsx
@@ -1,1 +1,58 @@
-export const BTCSlider = () => {};
+import { createPortal } from "react-dom";
+import type { PortalProps } from "../types";
+import { useEffect, useLayoutEffect, useRef, useState, type FC } from "react";
+
+export const Slider: FC<PortalProps> = ({ container }) => {
+  const portal = document.querySelector(container);
+  const [sliderValuePercent, setSliderValue] = useState<any>(50);
+  const sliderRef = useRef<HTMLInputElement>(null);
+  const tooltipRef = useRef<HTMLDivElement>(null);
+
+  const updateDivPosition = (value: number) => {
+    console.log({ value });
+    if (sliderRef.current && tooltipRef.current) {
+      const slider = sliderRef.current;
+      const tooltip = tooltipRef.current;
+      const position =
+        ((value - 1) / (100 - 1)) * (slider.offsetWidth - tooltip.offsetWidth);
+      tooltip.style.left = `${position}px`;
+      tooltip.style.display = "flex";
+
+      // Update the displayed value
+      //   document.getElementById("demo").innerText = value.toString();
+    }
+  };
+  useEffect(() => {
+    setSliderValue(90);
+  }, []);
+  useLayoutEffect(() => {
+    console.log("aici");
+    if (sliderRef.current) {
+      sliderRef.current.style.background = `linear-gradient(to right, #7b3dff ${sliderValuePercent}%, #ccc 0%)`;
+    }
+    updateDivPosition(sliderValuePercent);
+  }, [sliderValuePercent]);
+
+  return (
+    portal &&
+    createPortal(
+      <>
+        <input
+          type="range"
+          min="1"
+          max="100"
+          value={sliderValuePercent}
+          style={{ position: "relative" }}
+          className="slider"
+          id="myRange"
+          ref={sliderRef}
+          //   onChange={(e) => setSliderValue(e.target.value)} // Add this line to update sliderValue on change
+        />
+        <div className="tool" id="tooltip">
+          <span id="demo">50</span>Minimum tickets Left To Execute The Draw
+        </div>
+      </>,
+      portal
+    )
+  );
+};
